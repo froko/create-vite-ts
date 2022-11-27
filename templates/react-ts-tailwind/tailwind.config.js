6fe753f43af5a41dfed62770e11411130490a40f
Patrick Ineichen <patrick.ineichen@bbv.ch>
2022-11-27 23:03:48 +0100

    fix: 🐛 Add missing font family in react tailwind config

--- templates/react-ts-tailwind/tailwind.config.js
+++ templates/react-ts-tailwind/tailwind.config.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Avenir', 'Helvetica', 'Arial', 'sans-serif']
+    },
     extend: {}
   },
   plugins: []
