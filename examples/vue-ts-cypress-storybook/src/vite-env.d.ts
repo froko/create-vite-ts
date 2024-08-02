960bfaaab6222a8f119c24730c507856e0baf2c5
Patrick Ineichen <github@frokonet.ch>
2024-08-02 08:22:43 +0200

    chore(examples): update dependencies

--- examples/vue-ts-cypress-storybook/src/vite-env.d.ts
+++ examples/vue-ts-cypress-storybook/src/vite-env.d.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
 /// <reference types="vite/client" />
 /// <reference types="@histoire/plugin-vue/components" />
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
 
-  const component: DefineComponent<{}, {}, any>;
+  const component: DefineComponent<{}, {}, unknown>;
   export default component;
 }
